5ce20ce825905ee0970a75f33faf45153c831d57
pmontesano <pablo.montesano@mercadolibre.com>
2022-03-22 00:44:06 -0300

    ajustes estilos

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Navbar from './navbar';
 import ContainerPage from '../layout/container';
 import { Link } from 'react-router-dom';
+import useAuth from '../../auth/useAuth';
 
 const Header = () => {
+  const auth = useAuth();
+
+  const isLogged = auth?.isLogged;
+
   return (
-    <div className="header">
+    <div className='header'>
       <ContainerPage>
-        <div className="header_content">
-          <div className="logo">
-            <Link to="/">MonteMovies</Link>
+        <div
+          className={`header_content ${
+            !isLogged ? 'header_content-original' : ''
+          } `}
+        >
+          <div className='logo'>
+            <Link to='/'>MonteMovies</Link>
           </div>
           <Navbar />
         </div>
